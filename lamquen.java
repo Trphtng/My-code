39e95aee16dcc5ee1ff6a5318d776858fcf2f0c1
Trphtng <phatafwrk@gmail.com>
2024-08-08 13:14:36 +0700

    nhap

---
+++ lamquen.java
@@ -0,0 +1,11 @@
+package coban;
+
+public class lamquen {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+
+	}
+}
